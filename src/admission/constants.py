b06105fd092a7fcd8df5883449cbad5894613be1
Yvan <yguilvard@gmail.com>
2026-03-11 12:54:40 +0100

    Moved constants to a dedicated file

---
+++ src/admission/constants.py
@@ -0,0 +1,11 @@
+FEATURE_MAX_VALUES = {
+    "gre_score": 340.0,
+    "toefl_score": 120.0,
+    "rating": 5.0,
+    "sop": 5.0,
+    "lor": 5.0,
+    "cgpa": 10.0,
+    "research_xp": 1.0,
+}
+
+TARGET_MAX_VALUE = 1.0
